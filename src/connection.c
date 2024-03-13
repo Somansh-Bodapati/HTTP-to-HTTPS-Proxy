0682fe5f5561ec15d56fd20438ec81a5fadbcbc4
Somansh-Bodapati <somansh.bodapati@gmail.com>
2024-03-12 20:11:18 -0700

    Parsing request check

--- src/connection.c
+++ src/connection.c
@@ -60,7 +60,7 @@ void *handle_client(void *newsockfd) {
     // Handle the client request here
     printf("Handling request\n");
     HttpRequest req = parse_http_request(sockfd);
-    printf("Received Request: %s %s %s\n", req.method, req.uri, req.version);
+    printf("Received Request: %s %s %s\n", req.method, req.uri, req.httpVersion);
 
     close(sockfd);  // Close the client socket
     return NULL;    // Return from the thread
